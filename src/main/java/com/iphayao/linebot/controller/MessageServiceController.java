dd3784a13a8285cfba21c45d1f5d7fa3a43a4c87
DESKTOP-7EIVL73\johnyot <johnyoter@gmail.com>
2020-12-09 21:15:31 +0700

    add service push message http://localhost:8080/v1/postmsg

--- src/main/java/com/iphayao/linebot/controller/MessageServiceController.java
+++ src/main/java/com/iphayao/linebot/controller/MessageServiceController.java
@@ -1,5 +1,6 @@
 package com.iphayao.linebot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,13 +9,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.iphayao.linebot.model.MessageReq;
 import com.iphayao.linebot.model.MessageResp;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 
 @RestController
 @RequestMapping(value = "/v1")
 public class MessageServiceController {
 
+	@Value("${line.bot.channel-token}")
+	private String chToken;
+	
 	@PostMapping(value="/postmsg", consumes = "application/json", produces = "application/json")
-	public MessageResp newEmployee(@RequestBody MessageReq messageReq) {
-		return new MessageResp();
+	public BotApiResponse pushMessage(@RequestBody MessageReq messageReq) {
+		
+		final LineMessagingClient client = LineMessagingClient
+		        .builder(chToken)
+		        .build();
+
+		final TextMessage textMessage = new TextMessage(messageReq.getMessage());
+		final PushMessage pushMessage = new PushMessage(messageReq.getUserId(),  textMessage);
+
+		final BotApiResponse botApiResponse;
+		try {
+		    botApiResponse = client.pushMessage(pushMessage).get();
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    return null;
+		}
+
+		System.out.println(botApiResponse);
+		
+		return botApiResponse;
 	}
+	
 }
